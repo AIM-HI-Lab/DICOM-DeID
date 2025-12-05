cb8ebd6d2d0a09b7bdcc33760ceb20a7751d9d11
Nicholas Heller <nick7heller@gmail.com>
2025-12-05 14:33:53 -0600

    ud

--- dev/251205_incorporate_manifest_gen/s01_test_.sh
+++ dev/251205_incorporate_manifest_gen/s01_test_.sh
@@ -20,5 +20,5 @@ export CODE_PATH="/home/${USER}/isilon/code/ccf"
 # Load virtualenv for this project (also user specific)
 source ~/isilon/env/peds-3.9/bin/activate
 
-python3 "${CODE_PATH}/DICOM-DeID/dicom_deid/local_deid.py" \
+python3 "${CODE_PATH}/DICOM-DeID/scripts/local_deid.py" \
     --config "dev/251205_incorporate_manifest_gen/s01_config.yaml"
